e807a91bb6ff5300b786a85a5b69a7232a3cdecb
s.adhanissa <adhanissalsa@gmail.com>
2024-07-12 22:34:06 +0700

    Update Data Cleaning - World Layoffs.sql

--- Data Cleaning - World Layoffs.sql
+++ Data Cleaning - World Layoffs.sql
@@ -28,7 +28,7 @@ SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location,
 industry, total_laid_off, percentage_laid_off, `date`, stage,
-country, funds_raised_millions) AS row_num
+country, funds_raised) AS row_num
 FROM layoffs_staging
 )
 SELECT *
@@ -38,21 +38,22 @@ WHERE row_num > 1;
 -- Duplicates checkpoint
 SELECT *
 FROM layoffs_staging
-WHERE company = 'Casper';
+WHERE company = 'Beyond Meat';
 
 -- Create staging table for deleting duplicates
-CREATE TABLE `layoffs_staging2` (
+ CREATE TABLE `layoffs_staging2` (
   `company` text,
   `location` text,
   `industry` text,
-  `total_laid_off` int DEFAULT NULL,
+  `total_laid_off` text,
   `percentage_laid_off` text,
   `date` text,
   `stage` text,
   `country` text,
-  `funds_raised_millions` int DEFAULT NULL,
-  `row_num` INT
+  `funds_raised` text,
+  `row_num` int
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 SELECT *
 FROM layoffs_staging2;
@@ -62,7 +63,7 @@ SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location,
 industry, total_laid_off, percentage_laid_off, `date`, stage,
-country, funds_raised_millions) AS row_num
+country, funds_raised) AS row_num
 FROM layoffs_staging;
 
 -- Filtering duplicates
@@ -88,45 +89,52 @@ SELECT DISTINCT industry
 FROM layoffs_staging2
 ORDER BY 1;
 
--- Standardizing Crypto Industry
+-- Standardizing Industry for eBay
 SELECT *
 FROM layoffs_staging2
-WHERE industry LIKE 'Crypto%';
+WHERE company LIKE 'eBay';
 
 UPDATE layoffs_staging2
-SET industry = 'Crypto'
-WHERE industry LIKE 'Crypto%';
+SET industry = 'Retail'
+WHERE company LIKE 'eBay';
 
--- Issues checkpoint: Location (No issues)
+-- Issues checkpoint: Location
 SELECT DISTINCT location
 FROM layoffs_staging2
 ORDER BY 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE location LIKE 'Malmo';
+
+UPDATE layoffs_staging2
+SET location = 'Malmo'
+WHERE location LIKE 'MalmÃ¶%';
 
 -- Issues checkpoint: Country
 SELECT DISTINCT country
 FROM layoffs_staging2
 ORDER BY 1;
 
--- Standardizing country: United States.
-SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+SELECT *
 FROM layoffs_staging2
-ORDER BY 1;
-
-UPDATE layoffs_staging2
-SET country = TRIM(TRAILING '.' FROM country)
-WHERE country LIKE 'United States%';
+WHERE country = 'Sweden'
+ORDER BY location;
 
 -- Standardizing date column type
 SELECT `date`,
-STR_TO_DATE(`date`, '%m/%d/%Y')
+STR_TO_DATE(`date`, '%Y-%m-%d')
 FROM layoffs_staging2;
 
 UPDATE layoffs_staging2
-SET `DATE` = STR_TO_DATE(`date`, '%m/%d/%Y');
+SET `DATE` = STR_TO_DATE(`date`, '%Y-%m-%d');
 
 -- Change column type to date
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN funds_raised INT;
 
 -- 3. CHECK NULLs AND BLANKs
 -- Checking null column data
@@ -138,15 +146,15 @@ AND percentage_laid_off IS NULL;
 -- Blanks checkpoint
 SELECT *
 FROM layoffs_staging2
-WHERE industry IS NULL
-OR industry = '';
+WHERE funds_raised IS NULL
+OR funds_raised = '';
 
 SELECT *
 FROM layoffs_staging2
-WHERE company LIKE 'Bally%';
+WHERE company LIKE 'Appsmith';
 
 -- Populate blank/null industry with the existing one of the same company and location
-SELECT t1.industry, t2.industry
+SELECT *
 FROM layoffs_staging2 t1
 JOIN layoffs_staging2  t2
 	ON t1.company = t2.company
@@ -156,8 +164,8 @@ WHERE (t1.industry IS NULL OR t1.industry = '')
 
 -- Changing blank data into NULLs
 UPDATE layoffs_staging2
-SET industry = NULL
-WHERE industry = '';
+SET funds_raised = NULL
+WHERE funds_raised = '';
 
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2  t2
